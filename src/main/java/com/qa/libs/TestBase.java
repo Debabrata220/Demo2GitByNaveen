c5b258df4c16d5a6a914b54e9efca22e7877051b
91774 <91774@DevBratt>
2021-08-09 19:55:27 +0530

    second git proj saved by suresh's guidance

---
+++ src/main/java/com/qa/libs/TestBase.java
@@ -0,0 +1,7 @@
+package com.qa.libs;
+
+public class TestBase {
+	public void testBase() {
+		System.out.println("Test methods mentioned");
+	}
+}
---
+++ src/test/java/com/qa/tests/LoginTest.java
@@ -0,0 +1,18 @@
+package com.qa.tests;
+
+import org.testng.annotations.Test;
+
+public class LoginTest {
+	@Test
+	public void test1() {
+		System.out.println("Test1 done fine");
+	}
+	@Test
+	public void test2() {
+		System.out.println("Test2 done fine");
+	}
+	@Test
+	public void test3() {
+		System.out.println("Test3 done fine");
+	}
+}
